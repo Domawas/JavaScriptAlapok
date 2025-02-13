ab839e981a706b6e8cd92f85b950f85c6c904550
Domawas <dominknagy@gmail.com>
2025-02-13 11:32:10 +0100

    javascript

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import { kuytaLista } from "./adat.js";
 
-
 /*  html manipulacio 
 
 html dom - lekepezi a htmlt egy dokument object modell szerkezetbe
@@ -12,13 +11,16 @@ let koszonElem = document.getElementById("koszon");
 console.log(koszonElem);
 koszonElem.innerHTML = "Szép az élet!";
 
-
 /* Article lembe ird bele egy h2 tagbe az elso kutya nevet */
 let artElem = document.getElementById("art");
-console.log(artElem);
-artElem.innerHTML=`<div class ="kutya>"
+let txt = "";
+for (let index = 0; index < kuytaLista.length; i++) {
+  txt += `<div class ="kutya>"
                     <h2>${kuytaLista[0].nev}</h2>  
                     <p>${kuytaLista[0].nem}</p>
                     <p>kor:${kuytaLista[0].kor}</p>
                     <p>Súly:${kuytaLista[0].suly}</p>
-                </div>`
+                </div>`;
+}
+console.log(txt)
+artElem.innerHTML = txt;
