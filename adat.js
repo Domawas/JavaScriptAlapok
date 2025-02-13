7deb216ea091f2f751652ecf70780dca709b62ae
Domawas <dominknagy@gmail.com>
2025-02-13 11:21:39 +0100

    kutya lista

---
+++ adat.js
@@ -0,0 +1,5 @@
+export let kuytaLista = [
+    { nev: "Morzsa", nem: "kan", kor: 12, suly: 25 },
+    { nev: "Bali", nem: "kan", kor: 2, suly: 4 },
+    { nev: "Kini", nem: "szuka", kor: 7, suly: 10 },
+  ];
